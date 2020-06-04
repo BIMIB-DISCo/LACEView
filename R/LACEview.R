f11701c3e6479359828c6b1ba29042a1c547d257
bhaveshsunny60 <bhaveshsunny60@yahoo.com>
2020-06-04 23:16:41 +0530

    added cola layout

--- R/LACEview.R
+++ R/LACEview.R
@@ -5,11 +5,83 @@
 #' @import htmlwidgets
 #'
 #' @export
-LACEview <- function(message, width = NULL, height = NULL, elementId = NULL) {
+LACEview <- function(inference, width = NULL, height = NULL, elementId = NULL) {
+  B = inference$B
+  adj_matrix = array(0L,c((dim(B)[1]-1),(dim(B)[2]-1)))
+  rownames(adj_matrix) <- colnames(B)[(2:ncol(B))]
+  colnames(adj_matrix) <- colnames(B)[(2:ncol(B))]
 
+  for(rP in 2:(nrow(B))-1) {
+
+    for(rC in (rP+1):nrow(B)) {
+
+      if(all(B[rP,1:rP] == B[rC,1:rP]) && sum(B[rP,])==(sum(B[rC,])-1)) {
+
+        adj_matrix[(rP-1),(rC-1)] <- 1
+
+      }
+
+    }
+
+  }
+
+  adjMatrix_base <- adj_matrix
+
+  elements = list()
+  elements$nodes = list()
+  elements$edges = list()
+  for(row in 1:nrow(adjMatrix_base)) {
+    for(col in 1:ncol(adjMatrix_base)) {
+      if(adjMatrix_base[row, col] == 1){
+        data = list()
+        data$data = list()
+        data$data$source = sprintf('%s_t%s',clone_labels[row],1)
+        data$data$target = sprintf('%s_t%s',clone_labels[col],1)
+        data$data$id = sprintf('%s_%s',data$data$source,data$data$target)
+        data$data$linestyle = "solid"
+        elements$edges = c(elements$edges,list(data))
+      }
+    }
+  }
+  for(col in 1:(ncol(inference$clones_prevalence)-1)) {
+    data = list()
+    data$data = list()
+    data$data$id = sprintf('t%s',col)
+    data$data$name = sprintf('t%s',col)
+    parent = data$data$name
+    elements$nodes = c(elements$nodes,list(data))
+    for(row in 1:nrow(inference$clones_prevalence)) {
+      data = list()
+      data$data = list()
+      data$data$name = clone_labels[row]
+      data$data$id = sprintf('%s_t%s',clone_labels[row],col)
+      data$data$prev = sprintf('%s',round(inference$clones_prevalence[row,col],2))
+      data$data$parent = parent
+      elements$nodes = c(elements$nodes,list(data))
+    }
+  }
+  for(row in 1:(nrow(inference$clones_prevalence))) {
+    for(col in 1:(ncol(inference$clones_prevalence)-1)) {
+      if(col == 1){
+        source = sprintf('%s_t%s',clone_labels[row],col)
+        next
+      }
+      target = sprintf('%s_t%s',clone_labels[row],col)
+      data = list()
+      data$data = list()
+      data$data$source = source
+      data$data$id = sprintf('%s_%s',source,target)
+      data$data$target = target
+      data$data$linestyle = "dashed"
+      source = target
+      elements$edges = c(elements$edges,list(data))
+    }
+  }
+
+  print(elements)
   # forward options using x
   x = list(
-    message = message
+    elements = elements
   )
 
   # create widget
--- inst/htmlwidgets/LACEview.js
+++ inst/htmlwidgets/LACEview.js
@@ -13,101 +13,44 @@ HTMLWidgets.widget({
       renderValue: function(x) {
 
         // TODO: code to render the widget, e.g.
-
         var cy = window.cy = cytoscape({
           container: el,
 
-    boxSelectionEnabled: false,
+          boxSelectionEnabled: false,
 
-  style: [
-    {
-      selector: 'node',
-      css: {
-        'content': 'data(name)',
-        'text-valign': 'center',
-        'text-halign': 'center'
-      }
-    },
-    {
-      selector: ':parent',
-      css: {
-        'text-valign': 'top',
-        'text-halign': 'center',
-      }
-    },
-    {
-      selector: 'edge',
-      css: {
-        'curve-style': 'bezier',
-        'target-arrow-shape': 'triangle'
-      }
-    }
-  ],
+          style: [
+            {
+              selector: 'node',
+              css: {
+                'content': 'data(name)',
+                'text-valign': 'center',
+                'text-halign': 'center'
+              }
+            },
+            {
+              selector: ':parent',
+              css: {
+                'text-valign': 'top',
+                'background-opacity': '0.333',
+                'text-halign': 'center',
+              }
+            },
+            {
+              selector: 'edge',
+              css: {
+                'curve-style': 'bezier',
+                'target-arrow-shape': 'triangle',
+                'line-style': 'data(linestyle)'
+              }
+            }
+          ],
 
-  elements: {
-    nodes: [
-      { data: { id: 'HNRNPC',name:'HNRNPC', parent: 't1' }, position: { x: 135, y: 135 } },
-      { data: { id: 'PRAME',name:'PRAME', parent: 't1' }, position: { x: 215, y: 55 } },
-      { data: { id: 'ARPC2',name:'ARPC2', parent: 't1' }, position: { x: 305, y: 25 } },
-      { data: { id: 'COL1A2',name:'COL1A2', parent: 't1' }, position: { x: 215, y: 125 } },
-      { data: { id: 'RPL5',name:'RPL5', parent: 't1' }, position: { x: 315, y: 125 } },
-      { data: { id: 'CCT8',name:'CCT8', parent: 't1' }, position: { x: 185, y: 175 } },
-      { data: { id: 't1',name:'t1' } },
-      { data: { id: 'HNRNPC_t1',name:'HNRPC', parent: 't2' }, position: { x: 125, y: 245 } },
-      { data: { id: 'CCT8_t1',name:'CCT8', parent: 't2' }, position: { x: 185, y: 245 } },
-      { data: { id: 'COL1A2_t1',name:'COL1A2', parent: 't2' }, position: { x: 245, y: 245 } },
-      { data: { id: 'PRAME_t1',name:'PRAME', parent: 't2' }, position: { x: 305, y: 245 } },
-      { data: { id: 'ARPC2_t1',name:'ARPC2', parent: 't2' }, position: { x: 365, y: 245 } },
-      { data: { id: 'RPL5_t1',name:'RPL5', parent: 't2' }, position: { x: 425, y: 245 } },
-      { data: { id: 't2' ,name:'t2'} },
-      { data: { id: 'HNRNPC_t2',name:'HNRPC', parent: 't3' }, position: { x: 125, y: 315 } },
-      { data: { id: 'CCT8_t2',name:'CCT8', parent: 't3' }, position: { x: 185, y: 315 } },
-      { data: { id: 'COL1A2_t2',name:'COL1A2', parent: 't3' }, position: { x: 245, y: 315 } },
-      { data: { id: 'PRAME_t2',name:'PRAME', parent: 't3' }, position: { x: 305, y: 315 } },
-      { data: { id: 'ARPC2_t2',name:'ARPC2', parent: 't3' }, position: { x: 365, y: 315 } },
-      { data: { id: 'RPL5_t2',name:'RPL5', parent: 't3' }, position: { x: 425, y: 315 } },
-      { data: { id: 't3',name:'t3' } },
-      { data: { id: 'HNRNPC_t3',name:'HNRPC', parent: 't4' }, position: { x: 125, y: 395 } },
-      { data: { id: 'CCT8_t3',name:'CCT8', parent: 't4' }, position: { x: 185, y: 395 } },
-      { data: { id: 'COL1A2_t3',name:'COL1A2', parent: 't4' }, position: { x: 245, y: 395 } },
-      { data: { id: 'PRAME_t3',name:'PRAME', parent: 't4' }, position: { x: 305, y: 395 } },
-      { data: { id: 'ARPC2_t3',name:'ARPC2', parent: 't4' }, position: { x: 365, y: 395 } },
-      { data: { id: 'RPL5_t3',name:'RPL5', parent: 't4' }, position: { x: 425, y: 395 } },
-      { data: { id: 't4',name:'t4' } },
+          elements: x["elements"],
 
-    ],
-    edges: [
-    { data: { id: 'COL1A2_CCT8', source: 'COL1A2', target: 'CCT8' } },
-    { data: { id: 'PRAME_COL1A2', source: 'PRAME', target: 'COL1A2' } },
-    { data: { id: 'ARPC2_PRAME', source: 'ARPC2', target: 'PRAME' } },
-    { data: { id: 'ARPC2_RPL5', source: 'ARPC2', target: 'RPL5' } },
-    { data: { id: 'PRAME_HNRNPC', source: 'PRAME', target: 'HNRNPC' } },
-    { data: { id: 'HNRNPC_HNRNPC_t1_t2', source: 'HNRNPC', target: 'HNRNPC_t1' } },
-    { data: { id: 'HNRNPC_HNRNPC_t2_t3', source: 'HNRNPC_t1', target: 'HNRNPC_t2' } },
-    { data: { id: 'HNRNPC_HNRNPC_t3_t4', source: 'HNRNPC_t2', target: 'HNRNPC_t3' } },
-    { data: { id: 'CCT8_CCT8_t1_t2', source: 'CCT8', target: 'CCT8_t1' } },
-    { data: { id: 'CCT8_CCT8_t2_t3', source: 'CCT8_t1', target: 'CCT8_t2' } },
-    { data: { id: 'CCT8_CCT8_t3_t4', source: 'CCT8_t2', target: 'CCT8_t3' } },
-    { data: { id: 'COL1A2_COL1A2_t1_t2', source: 'COL1A2', target: 'COL1A2_t1' } },
-    { data: { id: 'COL1A2_COL1A2_t2_t3', source: 'COL1A2_t1', target: 'COL1A2_t2' } },
-    { data: { id: 'COL1A2_COL1A2_t3_t4', source: 'COL1A2_t2', target: 'COL1A2_t3' } },
-    { data: { id: 'PRAME_PRAME_t1_t2', source: 'PRAME', target: 'PRAME_t1' } },
-    { data: { id: 'PRAME_PRAME_t2_t3', source: 'PRAME_t1', target: 'PRAME_t2' } },
-    { data: { id: 'PRAME_PRAME_t3_t4', source: 'PRAME_t2', target: 'PRAME_t3' } },
-    { data: { id: 'ARPC2_ARPC2_t1_t2', source: 'ARPC2', target: 'ARPC2_t1' } },
-    { data: { id: 'ARPC2_ARPC2_t2_t3', source: 'ARPC2_t1', target: 'ARPC2_t2' } },
-    { data: { id: 'ARPC2_ARPC2_t3_t4', source: 'ARPC2_t2', target: 'ARPC2_t3' } },
-    { data: { id: 'RPL5_RPL5_t1_t2', source: 'RPL5', target: 'RPL5_t1' } },
-    { data: { id: 'RPL5_RPL5_t2_t3', source: 'RPL5_t1', target: 'RPL5_t2' } },
-    { data: { id: 'RPL5_RPL5_t3_t4', source: 'RPL5_t2', target: 'RPL5_t3' } },
-    ]
-  },
-
-  layout: {
-    name: 'preset',
-    padding: 5
-  }
-});
+          layout: {
+                    name: 'cola'
+                },
+        });
 
       },
 
